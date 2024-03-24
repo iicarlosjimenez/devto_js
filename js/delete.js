9bb09ebcbd326d3cc167978132ed975873c982f0
Molar <grubenmj@gmail.com>
2024-03-23 23:15:57 -0600

    The delete button was done and refresh after the accion

---
+++ js/delete.js
@@ -0,0 +1,8 @@
+/* 
+
+Borre una publicacion de la base de datos
+1 Identificar cual quiero borrar
+2 Sabiendo cual quiero borrar obtener el ID de la publicacion
+3 llamar a la API concatenando el ID de la publicacion + el link de la API para borrar
+
+*/
